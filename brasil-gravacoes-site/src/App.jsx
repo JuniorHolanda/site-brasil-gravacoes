84e1b489c12f8d71fa34ff925faf9bc50ef143e2
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-11-25 20:27:06 -0300

    header
    header estilizada e dinâmica

---
+++ brasil-gravacoes-site/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/main-header.jsx"
+
+function App() {
+
+    return (
+        <div className="main">
+            <Header/>
+        </div>
+  )
+}
+
+export default App

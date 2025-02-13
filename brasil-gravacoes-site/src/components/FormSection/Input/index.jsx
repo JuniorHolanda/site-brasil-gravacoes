9eaecccd2f76d521c2cc68bfff7299234c99db63
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2025-02-12 23:26:08 -0300

    preview da imagem inserido

--- brasil-gravacoes-site/src/components/FormSection/Input/index.jsx
+++ brasil-gravacoes-site/src/components/FormSection/Input/index.jsx
@@ -6,21 +6,21 @@ import { RiFileUploadLine } from "react-icons/ri";
 
 
 function Input ({labelTitle}) {
-
+  const lastWord = labelTitle.split(" ").pop();
   const [img, setImg] = useState(null);
-
+  
   function showImg (e) {
     if (e.target.files.length > 0) {
-      setImg(e.target.files[0])
+      const file = e.target.files[0];
+      const imgUrl = URL.createObjectURL(file)
+      setImg(imgUrl)
     }
   }
 
   return (
     <>
-      {img && 
-        <img src={img.src} alt="" />
-      }
       <h2>{labelTitle}</h2>
+      {img && <img src={img} alt={`${lastWord} do cliente`}/>}  
       
       <label>
         <RiFileUploadLine className={styles.icon}/>
